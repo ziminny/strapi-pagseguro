725db0d18459ac5d81b3aba7653291c8a04f0d86
Vagner Oliveira <ziminny@gmail.com>
2021-07-20 00:49:33 +0100

    node_modules

---
+++ lib/example.ts
@@ -0,0 +1,24 @@
+import express from "express"
+import {generateKey, generatePayment} from "./index"
+ 
+import { ConfigsPagseguro } from "./services/AxiosService"
+const app = express()
+
+ 
+app.use(express.json())
+app.post('/',async (req , res) => {
+  
+  const configs:ConfigsPagseguro = {
+    secreteKey:'your key'
+  } 
+  
+   const generateKeyResponse = await generateKey({...configs } ,req.body)
+   const generatePaymentResponse = await generatePayment({...configs },generateKeyResponse)
+   return res.json(generatePaymentResponse)
+})
+
+
+app.listen(4500,() => {
+  console.log('Server Up');
+  
+})
